b0a713e1e05ce109aa43eddb7531153a0b50980b
gowhale <gabewhale@gmail.com>
2020-12-14 09:20:59 +0000

    Created class for braille alphabet

---
+++ alphabet.py
@@ -0,0 +1,58 @@
+class Alphabet ():
+
+    alphabet_to_braille = {
+        "a": "100000",
+        "b": "101000",
+        "c": "110000",
+        "d": "110100",
+        "e": "100100",
+        "f": "111000",
+        "g": "111100",
+        "h": "101100",
+        "i": "011000",
+        "j": "011100",
+        "k": "100010",
+        "l": "100010",
+        "m": "110010",
+        "n": "110110",
+        "o": "100110",
+        "p": "111010",
+        "q": "111110",
+        "r": "101110",
+        "s": "011010",
+        "t": "011110",
+        "u": "100011",
+        "v": "101011",
+        "w": "011101",
+        "x": "110011",
+        "y": "110111",
+        "z": "100111",
+    }
+
+    def __init__(self):
+        print("Alphabet Generated")
+
+        braille_to_alphabet = {}
+
+        for key in self.alphabet_to_braille:
+
+            braille_to_alphabet[self.alphabet_to_braille[key]] = key
+
+        self.braille_to_alphabet = braille_to_alphabet
+
+    def get_braille_to_alphabet(self):
+        return self.braille_to_alphabet
+
+    def get_alphabet_to_braille(self):
+        return self.alphabet_to_braille
+
+    def translate_braille_to_alphabet(self,braille):
+        try:
+            return self.braille_to_alphabet[braille]
+        except KeyError:
+            return "NO CHAR FOUND"
+
+test = Alphabet()
+# print(test.get_alphabet_to_braille())
+# print(test.get_braille_to_alphabet())
+print(test.translate_braille_to_alphabet("111000"))
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from character import BrailleCharacter
 from time import sleep
+from alphabet import Alphabet
 
 current_char = BrailleCharacter()
+braille_alphabet = Alphabet
 
 for _ in range (100):
-    print(current_char.get_current_dots_hash())
+    current_dots_hash = (current_char.get_current_dots_hash())
+
     sleep(0.1)
