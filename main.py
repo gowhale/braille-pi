42f588f981efd8542110ea7fa78c6d49ed9bdb7b
Gabe <gabewhale@gmail.com>
2020-12-15 17:29:38 +0000

    New physical prototype created, software changes to adhere to new design. Plus minor fix on letters after user testing

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ current_char = BrailleCharacter()
 braille_alphabet = Alphabet()
 
 
-for _ in range(100):
+for _ in range(1000):
     current_dots_hash = (current_char.get_current_dots_hash())
     braille_translation = braille_alphabet.translate_braille_to_alphabet(
         current_dots_hash)
     print("{} : {}".format(current_dots_hash, braille_translation))
-    sleep(0.1)
+    sleep(1)
