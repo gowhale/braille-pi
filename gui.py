d0d858c5fbcf1452eec7196c34603bfb05f2e859
gowhale <gabewhale@gmail.com>
2020-12-16 15:23:55 +0000

    GUI Improved to now show braille cell and letter translated

--- gui.py
+++ gui.py
@@ -4,11 +4,14 @@ import sys
 from pygame.locals import QUIT
 
 
+
 class Gui ():
 
     width = 500
     height = 400
     window_caption = "Braille Pi"
+    dot_radius = 50
+    dot_border = 10
 
     dot_locations = {
         1: {
@@ -40,11 +43,42 @@ class Gui ():
 
     def __init__(self):
         pygame.init()
+        screen_info = (pygame.display.Info())
+        self.width = screen_info.current_w
+        self.height = screen_info.current_h
+#         print(width,height)
 
-        self.DISPLAY = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+
+        self.DISPLAY = pygame.display.set_mode((self.width, self.height))
         pygame.display.set_caption(self.window_caption)
         WHITE = (255, 255, 255)
         self.DISPLAY.fill(WHITE)
+        
+        row_two = int(self.height / 2)
+        row_one = int(row_two / 2)
+        row_three = int(row_two / 2 * 3)
+        
+        self.dot_locations[1]["y"] = row_one
+        self.dot_locations[2]["y"] = row_one
+        self.dot_locations[3]["y"] = row_two
+        self.dot_locations[4]["y"] = row_two
+        self.dot_locations[5]["y"] = row_three
+        self.dot_locations[6]["y"] = row_three
+        
+        cell_height = (row_three-row_one)
+        
+        col_one = int((self.width / 4) - (cell_height * 0.4))
+        col_two = int((self.width / 4) + (cell_height * 0.4))
+        
+        self.dot_locations[1]["x"] = col_one
+        self.dot_locations[2]["x"] = col_two
+        self.dot_locations[3]["x"] = col_one
+        self.dot_locations[4]["x"] = col_two
+        self.dot_locations[5]["x"] = col_one
+        self.dot_locations[6]["x"] = col_two
+        
+        
+        
 
     def update(self):
         for event in pygame.event.get():
@@ -57,7 +91,7 @@ class Gui ():
         pygame.quit()
         sys.exit()
 
-    def draw_dot_hash(self, dot_hash):
+    def draw_dot_hash(self, dot_hash, letter):
         WHITE = (255, 255, 255)
         self.DISPLAY.fill(WHITE)
 
@@ -68,12 +102,27 @@ class Gui ():
                 BLACK = (0, 0, 0)
                 if dot == "0":
                     pygame.draw.circle(
-                        self.DISPLAY, BLACK, (self.dot_locations[index]["x"], self.dot_locations[index]["y"]), 30, 5)
+                        self.DISPLAY, BLACK, (self.dot_locations[index]["x"], self.dot_locations[index]["y"]), self.dot_radius, self.dot_border)
                 elif dot == "1":
                     pygame.draw.circle(
-                        self.DISPLAY, BLACK, (self.dot_locations[index]["x"], self.dot_locations[index]["y"]), 30, 30)
+                        self.DISPLAY, BLACK, (self.dot_locations[index]["x"], self.dot_locations[index]["y"]), self.dot_radius, self.dot_radius)
 
             except KeyError:
                 print("Key Error")
+                
+        font_size = int(self.height/2)           
+        scale_text = pygame.font.Font('freesansbold.ttf', font_size)
+        current_temp = letter.upper()
+        text = "{}".format(current_temp)
+        text_surface = scale_text.render(text, True, (0, 0, 0))
+        text_surface, text_rect = text_surface, text_surface.get_rect()
+        text_x = self.width/4*3
+        text_y = self.height/2
+        text_rect.center = (text_x,text_y)
+        self.DISPLAY.blit(text_surface, text_rect)
+        
 
         self.update()
+        
+# test = Gui()
+# test.close_gui()
--- main.py
+++ main.py
@@ -4,11 +4,14 @@ from time import sleep
 from alphabet import Alphabet
 from gui import Gui
 import pygame
+from speech import Speech
 
 GPIO.setmode(GPIO.BCM)
 
 current_char = BrailleCharacter()
 braille_alphabet = Alphabet()
+
+speech = Speech()
 
 show_gui = True
 
@@ -23,11 +26,14 @@ previous_letter = "_"
 for _ in range(600):
     current_dots_hash = (current_char.get_current_dots_hash())
 
-    if show_gui:
-        display.draw_dot_hash(current_dots_hash)
-
     braille_translation = braille_alphabet.translate_braille_to_alphabet(
         current_dots_hash)
+
+
+    if show_gui:
+        display.draw_dot_hash(current_dots_hash, braille_translation)
+
+
 
     if previous_letter != braille_translation:
         speech.say(braille_translation)
