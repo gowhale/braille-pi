5e06ac0f9db8987bd0994a604f2efcbc73eb603b
gowhale <gabewhale@gmail.com>
2021-03-08 13:01:19 +0000

    Added reason to skip tests

--- src/tests/test_translator.py
+++ src/tests/test_translator.py
@@ -2,7 +2,8 @@ import pytest
 from src.learning.translator import Translator
 from src.interaction.interaction_module import Interaction
 
-@pytest.mark.timeout(5)
+# @pytest.mark.timeout(5)
+@pytest.mark.skip(reason="No way of mocking inputs yet.")
 def test_translator():
     i = Interaction()
     Translator(i)
