bce49e116800b1f01fd7b89861958357bf20426b
gowhale <gabewhale@gmail.com>
2021-03-11 14:03:23 +0000

    Refactored character unit test to work on raspberry pi

--- src/tests/test_character.py
+++ src/tests/test_character.py
@@ -1,6 +1,7 @@
 import pytest
 raspberry_pi_testing = True
 try:
+    import RPi.GPIO as GPIO
     from src.interaction.character import BrailleCharacter
 except ModuleNotFoundError as e:
     raspberry_pi_testing = False
@@ -11,4 +12,5 @@ except ModuleNotFoundError as e:
 # @pytest.mark.skip(reason="Only testable on raspberry-pi")
 @pytest.mark.skipif(raspberry_pi_testing == False, reason="Platform is not raspberry pi")
 def test_braille_character_class():
+    GPIO.setmode(GPIO.BCM)
     BrailleCharacter()
