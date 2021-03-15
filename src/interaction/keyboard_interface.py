6a521f3de4f8701377381ba8a6b4baba0c951c0a
gowhale <gabewhale@gmail.com>
2021-03-15 14:28:15 +0000

    Documented the keyboard_interface file

--- src/interaction/keyboard_interface.py
+++ src/interaction/keyboard_interface.py
@@ -1,17 +1,35 @@
 # Description: The aim of this file is to translate keyboard buttons into dots
 
-# Inverses the value assigned to the key: 0 -> 1, 1 -> 0
-def inverse_value(hashmap, key):
-    if hashmap[key]:
-        hashmap[key] = 0
+#
+def inverse_value(key_presses, key):
+    """Inverses the value assigned to the key: 0 -> 1, 1 -> 0
+    
+    Parameters:
+        key_presses (Dict)      Dictionary which counts amount of times keys pressed.
+        key         (String)    Letter of key which has been pressed.
+
+    Returns:
+        key_presses (Dict)      Dictionary which counts amount of times keys pressed."""
+
+    if key_presses[key]:
+        key_presses[key] = 0
     else:
-        hashmap[key] = 1
-    return hashmap
+        key_presses[key] = 1
+    return key_presses
 
 
-# Checks pygame's events to see what buttons have been pressed
+#
 # Returns the dotset
 def check_keys(pg, key_presses):
+    """Checks pygame's events to see what buttons have been pressed.
+
+    Parameters:
+        pg          (Pygame)    Pygame Module contains actions from pygame.
+        key_presses (Dict)      Dictionary which counts amount of times keys pressed.
+
+    Returns:
+        current_dots_hash   (String)    The hash calculated after inversing each key press."""
+
     events = pg.event.get()
     for event in events:
         if event.type == pg.KEYDOWN:
