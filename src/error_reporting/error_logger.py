534f91301b780e19f1cb7bd780bf0f059c691d97
gowhale <gabewhale@gmail.com>
2021-03-15 14:06:23 +0000

    Documentation added to the error logger.

--- src/error_reporting/error_logger.py
+++ src/error_reporting/error_logger.py
@@ -10,6 +10,17 @@ from datetime import datetime
 
 class ErrorLogger():
 
+    """The purpose of this class is to make a folder to store error messages in.
+
+    Attributes:
+        error_log_directory     (String)    Address of the log directory.
+        error_folder_name       (String)    Name of the error logs folder.
+        now                     (DateTime)  Current DateTime object.
+        date                    (String)    Just the date of the current DateTime object
+        error_file_name         (String)    Formatted name of the file.
+        log_file_path           (String)    Full path to where the file should be saved.
+    """
+
     error_log_directory = ""
     error_folder_name = 'error_logs'
     now = datetime.now()
@@ -18,6 +29,8 @@ class ErrorLogger():
     log_file_path = "{}/{}".format(error_folder_name, error_file_name)
 
     def __init__(self):
+        """Creates a folder for error logs if not already created."""
+
         print("Creating a folder here: {}".format(self.error_log_directory))
         if not os.path.exists(self.error_folder_name):
             os.makedirs(self.error_folder_name)
@@ -39,6 +52,12 @@ class ErrorLogger():
             file.close()
 
     def log_error(self, error):
+        """Logs an error message to the error file.
+
+        Parameters:
+            error   (String)    Description of what error happened.
+        """
+
         now = datetime.now()
         date = now.strftime("%d/%m/%Y")
         time = now.strftime("%H:%M:%S")
@@ -47,5 +66,3 @@ class ErrorLogger():
             writer = csv.writer(f)
             writer.writerow(fields)
         f.close()
-
-
