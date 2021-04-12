6ac9de0412ac9f6c03af812069f293891a099aeb
gowhale <gabewhale@gmail.com>
2021-04-12 13:10:31 +0100

    Changed variable names

--- src/results_logging/results_logger.py
+++ src/results_logging/results_logger.py
@@ -1,5 +1,5 @@
-# Program Name: error_logger.py
-# Description: This program will be used to log errros in a CSV file, it also generates a folder for the errors
+# Program Name: results_logger.py
+# Description: This program will be used to log errros in a CSV file, it also generates a folder for the quiz results
 
 
 import os
@@ -14,27 +14,27 @@ class ResultsLogger():
     """The purpose of this class is to make a folder to store results.
 
     Attributes:
-        error_log_directory     (String)    Address of the log directory.
-        error_folder_name       (String)    Name of the error logs folder.
+        results_log_directory   (String)    Address of the log directory.
+        results_folder_name     (String)    Name of the results logs folder.
         now                     (DateTime)  Current DateTime object.
         date                    (String)    Just the date of the current DateTime object
-        error_file_name         (String)    Formatted name of the file.
+        results_file_name       (String)    Formatted name of the file.
         log_file_path           (String)    Full path to where the file should be saved.
     """
 
-    error_log_directory = ""
-    error_folder_name = results_folder_name
+    results_log_directory = ""
+    results_folder_name = results_folder_name
     now = datetime.now()
     date = now.strftime("%d-%m-%Y")
-    error_file_name = "{}-QUIZ-RESULTS.csv".format(date)
-    log_file_path = "{}/{}".format(error_folder_name, error_file_name)
+    results_file_name = "{}-QUIZ-RESULTS.csv".format(date)
+    log_file_path = "{}/{}".format(results_folder_name, results_file_name)
 
     def __init__(self):
-        """Creates a folder for error logs if not already created."""
+        """Creates a folder for results logs if not already created."""
 
-        print("Creating a folder here: {}".format(self.error_log_directory))
-        if not os.path.exists(self.error_folder_name):
-            os.makedirs(self.error_folder_name)
+        print("Creating a folder here: {}".format(self.results_log_directory))
+        if not os.path.exists(self.results_folder_name):
+            os.makedirs(self.results_folder_name)
             print("QUIZ LOG CREATED")
         else:
             print("QUIZ LOG ALREADY EXISTS")
